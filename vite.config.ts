e7a0e2106b35f303aef3a8d6085ea9cba8ce9d9a
sandersaldev <developer.sandersal@gmail.com>
2025-10-16 15:34:15 -0300

    Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/admin', // <-- ADICIONE ISSO
+  base: '/', // <-- ADICIONE ISSO
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
